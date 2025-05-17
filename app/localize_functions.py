b189e3e23a9d2ef4fd3f06fe6003b7e07a412c6c
AntoArroyo <arroyo.antom@gmail.com>
2025-05-17 23:31:03 +0200

    Fix localize function

--- app/localize_functions.py
+++ app/localize_functions.py
@@ -1,16 +1,20 @@
 def rssi_similarity(rssi1, rssi2):
     # Smaller difference means more similar (using inverse of distance)
-    return 1 / (1 + abs(rssi1 - rssi2))
+    if rssi1 is not None and rssi2 is not None:
+        return 1 / (1 + abs(rssi1 - rssi2))
+    else: 
+        return 0
 
-def compute_similarity(scan_signals: dict, db_signals: dict):
+def compute_similarity(db_signals: dict, scan_signals: dict):
     score = 0.0
     x, y, z = None, None, None
     
     for position in db_signals:
         for signal in scan_signals:
             for device in position.get('WiFi'):
-                if signal.bssid in device.get('BSSID'):
-                    score += rssi_similarity(signal.rssi, db_signals[signal.mac_address])
+                if signal.bssid == device.get('BSSID'):
+                    print(f" RSSI SCANNED {signal.bssid} RSSI STORED {device.get('BSSID')}")
+                    score += rssi_similarity(signal.rssi, device.get('RSSI'))
                     x = position.get('Position').get('X')
                     y = position.get('Position').get('Y')
                     z = position.get('Position').get('Z')
@@ -22,7 +26,7 @@ def compute_best_position_basic(wifi_signals: list, scanned_signals: list):
     best_position = None
 
     # similarity between the positions and wifi received and stored
-    score, x, y, z = compute_similarity(scanned_signals, wifi_signals)
+    score, x, y, z = compute_similarity(wifi_signals, scanned_signals )
 
     if score > best_score:
         best_score = score
