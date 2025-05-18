75c3507dcbe2cb07ba9d15754957d455ad251203
AntoArroyo <arroyo.antom@gmail.com>
2025-05-18 18:28:36 +0200

    Fix database url to load from .env files

--- app/auth.py
+++ app/auth.py
@@ -4,7 +4,7 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 import os
 
 load_dotenv()
--- app/database.py
+++ app/database.py
@@ -1,11 +1,28 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/fastapi_db")
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+
+if not POSTGRES_USER or not POSTGRES_PASSWORD:
+    raise EnvironmentError("POSTGRES_USER and POSTGRES_PASSWORD must be set.")
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/fastapi_db"
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
